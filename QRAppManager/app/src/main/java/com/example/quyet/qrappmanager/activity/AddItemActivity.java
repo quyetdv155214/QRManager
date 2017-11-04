06d0a9bc1a0afcae18e731f130a7acd4cc205061
Tyeuq <quyetdv15524@gmail.com>
2017-11-04 19:15:41 +0700

    Login success

--- QRAppManager/app/src/main/java/com/example/quyet/qrappmanager/activity/AddItemActivity.java
+++ QRAppManager/app/src/main/java/com/example/quyet/qrappmanager/activity/AddItemActivity.java
@@ -3,14 +3,17 @@ package com.example.quyet.qrappmanager.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.View;
 
 import com.example.quyet.qrappmanager.R;
 import com.example.quyet.qrappmanager.databinding.ActivityAddItemBinding;
+import com.example.quyet.qrappmanager.model.Item;
 import com.example.quyet.qrappmanager.viewmodel.ActivityAddItemViewModel;
 
 
 
-public class AddItemActivity extends BaseActivity<ActivityAddItemBinding, ActivityAddItemViewModel> {
+public class AddItemActivity extends BaseActivity<ActivityAddItemBinding, ActivityAddItemViewModel> implements View.OnClickListener{
+
 
     @Override
     public int getLayoutId() {
@@ -36,9 +39,40 @@ public class AddItemActivity extends BaseActivity<ActivityAddItemBinding, Activi
         getSupportActionBar().hide();
         setStatusBarTranslucent(true);
         setUpView();
+        setEvent();
+        getData();
+    }
+
+    private void setEvent() {
+        getBinding().ivAddOK.setOnClickListener(this);
+        getBinding().ivDefaultAddItemback.setOnClickListener(this);
+    }
+
+    private Item getData() {
+        String itemName = getBinding().etItemName.getText().toString();
+        float itemPrice= Float.parseFloat(getBinding().etItemPrice.getText().toString());
+        String itemDes= getBinding().etItemDes.getText().toString();
+        String itemInfo = getBinding().etIteminfo.getText().toString();
+        return null;
     }
 
     private void setUpView() {
         getBinding().etItemPrice.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ivAddOK :
+                addItem();
+                break;
+            case R.id.ivDefaultAddItemback :
+                onBackPressed();
+                break;
+        }
+    }
+
+    private void addItem() {
+        Item item = getData();
     }
 }
--- QRAppManager/app/src/main/java/com/example/quyet/qrappmanager/activity/LoginActivity.java
+++ QRAppManager/app/src/main/java/com/example/quyet/qrappmanager/activity/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.quyet.qrappmanager.activity;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,11 +13,24 @@ import com.andexert.library.RippleView;
 import com.example.quyet.qrappmanager.BR;
 import com.example.quyet.qrappmanager.R;
 import com.example.quyet.qrappmanager.databinding.ActivityLoginBinding;
+import com.example.quyet.qrappmanager.networks.NetContext;
+import com.example.quyet.qrappmanager.networks.jsonmodel.LoginBodyJson;
+import com.example.quyet.qrappmanager.networks.jsonmodel.LoginResponseJson;
+import com.example.quyet.qrappmanager.networks.services.LoginService;
+import com.example.quyet.qrappmanager.ultil.Constant;
 import com.example.quyet.qrappmanager.viewmodel.ActivityHomeViewModel;
 import com.example.quyet.qrappmanager.viewmodel.ActivityLoginViewModel;
+import com.google.gson.Gson;
+
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends BaseActivity<ActivityLoginBinding, ActivityLoginViewModel>  {
-
+    public String managerId;
+    public String resID;
     private static final String TAG = "loginActivity";
 
     private ProgressDialog pDialog;
@@ -43,22 +57,33 @@ public class LoginActivity extends BaseActivity<ActivityLoginBinding, ActivityLo
     public void init(){
         getSupportActionBar().hide();
         setStatusBarTranslucent(true);
-        pDialog = new ProgressDialog(this);
-        pDialog.setCancelable(false);
+
         setEvent();
+        setUI();
         Log.d(TAG, "init: init done");
     }
 
-    public void onLoginSuccess() {
-        getBinding().rlLogin.setEnabled(true);
-//        finish();
+    private void setUI() {
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Logging...");
+        pDialog.setCancelable(false);
+        pDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+    }
+
+    public void onLoginSuccess(String managerId) {
+//        getBinding().rlLogin.setEnabled(true);
         Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
 
     public void onLoginFailed() {
         Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-
         getBinding().rlLogin.setEnabled(true);
     }
     public boolean validate() {
@@ -92,8 +117,10 @@ public class LoginActivity extends BaseActivity<ActivityLoginBinding, ActivityLo
     private void setEvent(){
 //        getBinding().rlLogin.setOnRippleCompleteListener(this);
         getBinding().rlLogin.setOnRippleCompleteListener(new RippleView.OnRippleCompleteListener() {
+
             @Override
             public void onComplete(RippleView rippleView) {
+                getBinding().btnLogin.setEnabled(false);
                 login();
             }
         });
@@ -121,24 +148,53 @@ public class LoginActivity extends BaseActivity<ActivityLoginBinding, ActivityLo
             return;
         }
 
-        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
-                ProgressDialog.STYLE_HORIZONTAL);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Authenticating...");
-        progressDialog.show();
+        pDialog.show();
+
 
         String email = getBinding().inputEmail.getText().toString();
         String password = getBinding().inputPassword.getText().toString();
+//        String email = "myemail3@gmail.com";
+//        String password = "123456";
+
 
         // TODO: Implement login code
 
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        onLoginSuccess();
-                        progressDialog.dismiss();
+
+        LoginService loginService = NetContext.instance.create(LoginService.class);
+        MediaType jsonType = MediaType.parse("application/json");
+
+        final String loginJson = (new Gson()).toJson(new LoginBodyJson(email, password));
+        Log.d(TAG, "loginJson: "+loginJson.toString());
+        final RequestBody loginBody = RequestBody.create(jsonType, loginJson);
+        Log.d(TAG, "loginBody: " +loginBody.toString());
+        Call<LoginResponseJson> loginCall = loginService.login(loginBody);
+        Log.d(TAG, "loginCall: " + loginCall.toString());
+
+        loginCall.enqueue(new Callback<LoginResponseJson>() {
+            @Override
+            public void onResponse(Call<LoginResponseJson> call, Response<LoginResponseJson> response) {
+                pDialog.dismiss();
+
+                LoginResponseJson loginResponseJson = response.body();
+                if(loginResponseJson !=null){
+                    if(response.code() ==200){
+                        managerId = loginResponseJson.getManagerId();
+                        onLoginSuccess(managerId);
                     }
-                }, 3000);
+                }else {
+                    Toast.makeText(LoginActivity.this, Constant.LOGIN_WRONG_ACCOUNT_MESS, Toast.LENGTH_SHORT).show();
+                    onLoginFailed();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LoginResponseJson> call, Throwable t) {
+                pDialog.dismiss();
+                onLoginFailed();
+                Log.d(TAG, String.format("onFailure: %s", t));
+            }
+        });
+
 
     }
     private void showDialog() {
